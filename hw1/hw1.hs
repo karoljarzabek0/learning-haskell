02946982630c01267d2c56e68dc820385028e98e
Karol Jarząbek <183915723+karoljarzabek0@users.noreply.github.com>
2026-05-30 00:39:46 +0200

    progress on the homework!

--- hw1/hw1.hs
+++ hw1/hw1.hs
@@ -1,10 +1,31 @@
 -- Homework1
 -- Credit card validator
 
-firstDigit :: Int = 1
+firstDigit :: Integer = 1234
 
-inputList :: [Int] = [1, 2, 3, 4]
+inputList :: [Integer] = [1, 2, 3, 4, 5]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+    | n < 1 = []
+    | n < 10 = [n]
+    | otherwise = mod n 10 : toDigitsRev (n `div` 10)
+
+toDigits :: Integer -> [Integer]
+toDigits n
+    | n < 1 = []
+    | n < 10 = [n]
+    | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x : (y : zs)) = x * 2 : y : doubleEveryOther zs
 
 main = do
-    let tList = filter odd inputList :: [Int]
-    print tList
+    let tList = filter odd inputList :: [Integer]
+    --    print tList
+    print (doubleEveryOther inputList)
+
+    print (toDigitsRev firstDigit)
+    print (toDigits firstDigit)
