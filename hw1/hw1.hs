d2d0fae25b7372c966b5f9b07723f86359195162
Karol Jarząbek <183915723+karoljarzabek0@users.noreply.github.com>
2026-05-30 12:45:56 +0200

    finished ex4

--- hw1/hw1.hs
+++ hw1/hw1.hs
@@ -1,31 +1,65 @@
 -- Homework1
 -- Credit card validator
+{- HLINT ignore "Use foldr" -}
 
 firstDigit :: Integer = 1234
+secondDigit :: Integer = 8765
 
 inputList :: [Integer] = [1, 2, 3, 4, 5]
 
+-- This function works
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
     | n < 1 = []
     | n < 10 = [n]
     | otherwise = mod n 10 : toDigitsRev (n `div` 10)
 
+-- This function works
 toDigits :: Integer -> [Integer]
 toDigits n
     | n < 1 = []
     | n < 10 = [n]
     | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (x : xs) = 1 + intListLength xs
+
+-- This works
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther [x] = [x]
-doubleEveryOther (x : (y : zs)) = x * 2 : y : doubleEveryOther zs
+doubleEveryOther (a : b : cs)
+    | even (intListLength (a : b : cs)) = a * 2 : b : doubleEveryOther cs
+    | otherwise = a : b * 2 : doubleEveryOther cs
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits [x] = sum (toDigits x)
+sumDigits (x : xs) = sumDigits [x] + sumDigits xs
+
+validate :: Integer -> Bool
+validate n
+    | sumDigits (doubleEveryOther (toDigits n)) `mod` 10 == 0 = True
+    | otherwise = False
+
+no1 = 4012888888881881
+no2 = 4012888888881882
 
 main = do
-    let tList = filter odd inputList :: [Integer]
+    -- let tList = filter odd inputList :: [Integer]
     --    print tList
-    print (doubleEveryOther inputList)
 
     print (toDigitsRev firstDigit)
     print (toDigits firstDigit)
+
+    print (toDigits secondDigit)
+
+    print "--------------------"
+    print (doubleEveryOther inputList)
+    print (doubleEveryOther (toDigits secondDigit))
+    print (doubleEveryOther [1, 2, 3])
+    print "--------------------"
+
+    print $ validate no1
+    print $ validate no2
